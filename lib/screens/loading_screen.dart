1718c50ec09b69dc8351f802bd19c495c604774d
FreshDev <billyhans90@gmail.com>
2022-08-22 04:31:59 +0100

    Network errors

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geolocator_android/geolocator_android.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -26,8 +27,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getData() async {
+    Future<http.Response> response = (await http.get(Uri.parse(
+            'https://api.openweathermap.org/data/2.5/weather?lat=43.497875050000005&lon=16.300506692819848&appid=253211610f895c1f5e2cb1f94903adfd')))
+        as Future<http.Response>;
+    print(response);
+  }
+  // Future<String> getData() async {
+  //   final response = await http.get(Uri.parse(
+  //       'https://api.openweathermap.org/data/2.5/weather?lat=43.497875050000005&lon=16.300506692819848&appid=253211610f895c1f5e2cb1f94903adfd'));
+  //   return response.body;
+  // }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
