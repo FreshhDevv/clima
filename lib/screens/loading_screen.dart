70ef1ac47a11b35db853a713673ac2b52a146b66
FreshDev <billyhans90@gmail.com>
2022-08-23 20:35:48 +0100

    Solved the fetching of data from the internet bug

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -3,6 +3,7 @@ import 'package:geolocator/geolocator.dart';
 import 'package:geolocator_android/geolocator_android.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -28,15 +29,24 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getData() async {
-    Future<http.Response> response = (await http.get(Uri.parse(
-            'https://api.openweathermap.org/data/2.5/weather?lat=43.497875050000005&lon=16.300506692819848&appid=253211610f895c1f5e2cb1f94903adfd')))
-        as Future<http.Response>;
-    print(response);
+    http.Response response = await http.get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=43.497875050000005&lon=16.300506692819848&appid=253211610f895c1f5e2cb1f94903adfd'));
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
   }
   // Future<String> getData() async {
   //   final response = await http.get(Uri.parse(
   //       'https://api.openweathermap.org/data/2.5/weather?lat=43.497875050000005&lon=16.300506692819848&appid=253211610f895c1f5e2cb1f94903adfd'));
   //   return response.body;
+  // }
+
+  // Future<http.Response> getData() async {
+  //   return http.get(Uri.parse(
+  //       'https://api.openweathermap.org/data/2.5/weather?lat=43.497875050000005&lon=16.300506692819848&appid=253211610f895c1f5e2cb1f94903adfd'));
   // }
 
   @override
