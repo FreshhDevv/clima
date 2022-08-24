17071d1ee6f9843fb29e6d17fcce2f5e6e59f36d
FreshDev <billyhans90@gmail.com>
2022-08-24 23:19:29 +0100

    Json parsing and dynamic types

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -3,6 +3,7 @@ import 'package:geolocator/geolocator.dart';
 import 'package:geolocator_android/geolocator_android.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:http/http.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -30,10 +31,18 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void getData() async {
     http.Response response = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat=43.497875050000005&lon=16.300506692819848&appid=253211610f895c1f5e2cb1f94903adfd'));
+        'https://api.openweathermap.org/data/2.5/weather?lat=4.155966&lon=9.263224&appid=253211610f895c1f5e2cb1f94903adfd'));
     if (response.statusCode == 200) {
       String data = response.body;
-      print(data);
+
+      var decodedData = jsonDecode(data);
+
+      double temperature = decodedData['main']['temp'];
+      int weatherConditionNumber = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+      print(temperature);
+      print(weatherConditionNumber);
+      print(cityName);
     } else {
       print(response.statusCode);
     }
