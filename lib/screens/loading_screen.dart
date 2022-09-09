931011ef5a44c168639b2730f5eca1d2d5a7c13b
FreshDev <billyhans90@gmail.com>
2022-09-09 20:17:44 +0100

    Refactoring

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:geolocator_android/geolocator_android.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 
@@ -35,16 +34,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
     var weatherData = await networkHelper.getData();
   }
-  // Future<String> getData() async {
-  //   final response = await http.get(Uri.parse(
-  //       'https://api.openweathermap.org/data/2.5/weather?lat=43.497875050000005&lon=16.300506692819848&appid=253211610f895c1f5e2cb1f94903adfd'));
-  //   return response.body;
-  // }
-
-  // Future<http.Response> getData() async {
-  //   return http.get(Uri.parse(
-  //       'https://api.openweathermap.org/data/2.5/weather?lat=43.497875050000005&lon=16.300506692819848&appid=253211610f895c1f5e2cb1f94903adfd'));
-  // }
 
   @override
   Widget build(BuildContext context) {
